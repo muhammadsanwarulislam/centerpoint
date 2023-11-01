b2bad0da952518ce81e3ee6250c864e46b89a197
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2023-11-01 23:03:56 +0600

    ui design

--- client/components/Sidebar.vue
+++ client/components/Sidebar.vue
@@ -3,9 +3,7 @@
 </script>
 <template>
     <aside class="sticky top-[65px] h-[calc(100vh-65px)] py-5">
-      <div
-        class="flex h-full w-full flex-col gap-5 overflow-y-auto rounded-md border bg-card pb-5 scrollbar-thin scrollbar-thumb-input scrollbar-thumb-rounded-md"
-      >
+      <div class="flex h-full w-full flex-col gap-5 overflow-y-auto rounded-md border bg-card pb-5 scrollbar-thin scrollbar-thumb-input scrollbar-thumb-rounded-md">
       <SidebarItem />
         <div class="mt-auto">
         </div>
