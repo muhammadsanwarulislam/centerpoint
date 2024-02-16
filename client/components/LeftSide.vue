d77611f99e9c7e98fafb3a43caed6435b994bef8
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2024-02-16 17:42:23 +0600

    dynamic menu list

--- client/components/LeftSide.vue
+++ client/components/LeftSide.vue
@@ -1,19 +1,39 @@
 <script setup>
 // Load Dynamic Component
-const { getComponent } = storeUserData()
+const { getComponent } = storeUserData();
+const component_id = ref(storeUserData().get_component || "Overview");
+const openSubmenu = ref(null);
+
+watch(
+    () => storeUserData().get_component,
+    (value) => {
+        component_id.value = value;
+    }
+);
+
+const menus =
+    storeUserData().user_data.user.role.get_menus_according_to_the_role_id;
+
 function loadComponent(component) {
-    getComponent(component)
+    getComponent(component);
 }
 
-const component_id = ref(storeUserData().get_component || 'Overview')
-watch(() => storeUserData().get_component, (value) => {
-    component_id.value = value
-})
+function loadComponentOrSubMenu(menu) {
+    if (hasSubmenu(menu.id)) {
+        toggleSubMenu(menu.id);
+    } else {
+        loadComponent(menu.component);
+    }
+}
 
-const menus = storeUserData().user_data.user.menus
-const source_index = ref(null)
+function toggleSubMenu(menuId) {
+    openSubmenu.value = openSubmenu.value === menuId ? null : menuId;
+}
+
+function hasSubmenu(menuId) {
+    return menus.some((menu) => menu.pivot.parent_id === menuId);
+}
 </script>
-
 <template>
     <div>
         <!-- Left Side Start -->
@@ -24,109 +44,53 @@ const source_index = ref(null)
                 <a href="/" class="flex items-center ps-2.5 mb-5">
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Centerpoint</span>
                 </a>
-                <ul class="space-y-2 font-medium">
-                    <li>
-                        <a @click="loadComponent('Overview')"
-                            :class="{ 'bg-gray-100 dark:bg-gray-700': component_id == 'Overview' }"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                viewBox="0 0 22 21">
-                                <path
-                                    d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                <path
-                                    d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                            </svg>
-                            <span class="ms-3">Overview</span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a @click="loadComponent('MenuCreation')"
-                            :class="{ 'bg-gray-100 dark:bg-gray-700': component_id == 'MenuCreation' }"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                viewBox="0 0 18 18">
-                                <path
-                                    d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                            </svg>
-                            <span class="flex-1 ms-3 whitespace-nowrap">Menu</span>
-                            <span
-                                class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a @click="loadComponent('UserCreation')"
-                            :class="{ 'bg-gray-100 dark:bg-gray-700': component_id == 'UserCreation' }"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                viewBox="0 0 18 20">
-                                <path
-                                    d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                            </svg>
-                            <span class="flex-1 ms-3 whitespace-nowrap">User Creation</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a @click="loadComponent('RoleCreation')"
-                            :class="{ 'bg-gray-100 dark:bg-gray-700': component_id == 'RoleCreation' }"
-                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                viewBox="0 0 18 20">
-                                <path
-                                    d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                            </svg>
-                            <span class="flex-1 ms-3 whitespace-nowrap">Role Creation</span>
-                        </a>
-                    </li>
-                    
-                    <li v-for="(item, index) in menus" :key="index">
-                        <!-- if item.component not null then add @click -->
-                        <button type="button"
-                            @click="item.component ? loadComponent(item.component) : '', source_index = source_index == index ? -1 : index"
-                            :class="{ 'bg-gray-100 dark:bg-gray-700': component_id == item.component }"
-                            class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                            
-                        >
-                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                viewBox="0 0 18 18">
-                                <path
-                                    d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                            </svg>
-                            <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{item.name}}</span>
-                            <!-- if item.menu_items then show svg  -->
-                            <svg v-if="item.menu_items.length > 0"
-                            class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="m1 1 4 4 4-4" />
-                            </svg>
-                        </button>
-                        <ul :class="source_index == index ? '' : 'hidden'">
-                            <li v-for="submenus in item.menu_items" :key="submenus.id">
-                                <a @click="loadComponent(submenus.component)"
-                                    :class="{ 'bg-gray-100 dark:bg-gray-700': component_id == submenus.component }"
-                                    class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-7 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white mr-2"
-                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                        viewBox="0 0 18 18">
-                                        <path
-                                            d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                                    </svg>
-                                    {{submenus.name}}
-                                </a>
+                <ul>
+                    <!-- Loop through top-level menus -->
+                    <template v-for="(item, index) in menus">
+                        <!-- Check if the menu item has a parent -->
+                        <template v-if="!item.pivot.parent_id">
+                            <li :key="index">
+                                <button type="button" @click="loadComponentOrSubMenu(item)" :class="{
+                                    'bg-gray-100 dark:bg-gray-700':
+                                        component_id === item.component,
+                                }" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ item.name
+                                    }}</span>
+                                    <span v-if="hasSubmenu(item.id)" class="mr-2">
+                                        <svg :class="{
+                                            'transform rotate-90': openSubmenu === item.id,
+                                        }" class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                            viewBox="0 0 22 21">
+                                            <path
+                                                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                                            <path
+                                                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                                        </svg>
+                                    </span>
+                                </button>
+                                <!-- Render child menus -->
+                                <ul v-if="openSubmenu === item.id">
+                                    <template v-for="(child, childIndex) in menus">
+                                        <li v-if="child.pivot.parent_id === item.id" :key="childIndex">
+                                            <button type="button" @click="loadComponent(child.component)" :class="{
+                                                'bg-gray-100 dark:bg-gray-700':
+                                                    component_id === child.component,
+                                            }" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                                <span class="flex-1 ms-6 text-left rtl:text-right whitespace-nowrap">{{
+                                                    child.name }}</span>
+                                            </button>
+                                        </li>
+                                    </template>
+                                </ul>
                             </li>
-                        </ul>
-                    </li>
+                        </template>
+                    </template>
                 </ul>
             </div>
         </aside>
         <!-- Left Side End -->
     </div>
 </template>
-
 <style></style>
+  
