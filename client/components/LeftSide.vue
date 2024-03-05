98cea7bfdf982f416751dc1f2abcbb854a3f7b41
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2024-03-05 13:56:04 +0600

    update menu

--- client/components/LeftSide.vue
+++ client/components/LeftSide.vue
@@ -5,90 +5,128 @@ const component_id = ref(storeUserData().get_component || "Overview");
 const openSubmenu = ref(null);
 
 watch(
-    () => storeUserData().get_component,
-    (value) => {
-        component_id.value = value;
-    }
+  () => storeUserData().get_component,
+  (value) => {
+    component_id.value = value;
+  }
 );
 const menus = storeUserData().user_data.role.get_menus_according_to_the_role_id;
 
 function loadComponent(component) {
-    getComponent(component);
+  getComponent(component);
 }
 
 function loadComponentOrSubMenu(menu) {
-    if (hasSubmenu(menu.id)) {
-        toggleSubMenu(menu.id);
-    } else {
-        loadComponent(menu.component);
-    }
+  if (hasSubmenu(menu.id)) {
+    toggleSubMenu(menu.id);
+  } else {
+    loadComponent(menu.component);
+  }
 }
 
 function toggleSubMenu(menuId) {
-    openSubmenu.value = openSubmenu.value === menuId ? null : menuId;
+  openSubmenu.value = openSubmenu.value === menuId ? null : menuId;
 }
 
 function hasSubmenu(menuId) {
-    return menus.some((menu) => menu.pivot.parent_id === menuId);
+  return menus.some((menu) => menu.pivot.parent_id === menuId);
 }
 </script>
+
 <template>
-    <div>
-        <!-- Left Side Start -->
-        <aside id="logo-sidebar"
-            class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 sidebar"
-            aria-label="Sidebar">
-            <div class="h-full px-3 pb-4 pt-4 overflow-y-auto bg-white dark:bg-gray-800">
-                <a href="/" class="flex items-center ps-2.5 mb-5">
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Centerpoint</span>
-                </a>
-                <ul>
-                    <!-- Loop through top-level menus -->
-                    <template v-for="(item, index) in menus">
-                        <!-- Check if the menu item has a parent -->
-                        <template v-if="!item.pivot.parent_id">
-                            <li :key="index">
-                                <button type="button" @click="loadComponentOrSubMenu(item)" :class="{
-                                    'bg-gray-100 dark:bg-gray-700':
-                                        component_id === item.component,
-                                }" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ item.name
-                                    }}</span>
-                                    <span v-if="hasSubmenu(item.id)" class="mr-2">
-                                        <svg :class="{
-                                            'transform rotate-90': openSubmenu === item.id,
-                                        }" class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                                            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                            viewBox="0 0 22 21">
-                                            <path
-                                                d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                            <path
-                                                d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                                        </svg>
-                                    </span>
-                                </button>
-                                <!-- Render child menus -->
-                                <ul v-if="openSubmenu === item.id">
-                                    <template v-for="(child, childIndex) in menus">
-                                        <li v-if="child.pivot.parent_id === item.id" :key="childIndex">
-                                            <button type="button" @click="loadComponent(child.component)" :class="{
-                                                'bg-gray-100 dark:bg-gray-700':
-                                                    component_id === child.component,
-                                            }" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                                <span class="flex-1 ms-6 text-left rtl:text-right whitespace-nowrap">{{
-                                                    child.name }}</span>
-                                            </button>
-                                        </li>
-                                    </template>
-                                </ul>
-                            </li>
-                        </template>
-                    </template>
+  <div>
+    <!-- Left Side Start -->
+    <aside
+      id="separator-sidebar"
+      class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 sidebar"
+      aria-label="Sidebar"
+    >
+      <div
+        class="h-full px-3 pb-4 pt-4 overflow-y-auto bg-white dark:bg-gray-800"
+      >
+        <a href="/" class="flex items-center ps-2.5 mb-5">
+          <span
+            class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+            >Centerpoint</span
+          >
+        </a>
+        <ul class="space-y-2 font-medium">
+          <!-- Loop through top-level menus -->
+          <template v-for="(item, index) in menus">
+            <!-- Check if the menu item has a parent -->
+
+            <template v-if="!item.pivot.parent_id">
+              <li :key="index">
+                <button
+                  type="button"
+                  @click="loadComponentOrSubMenu(item)"
+                  :class="{
+                    'bg-gray-100 dark:bg-gray-700':
+                      component_id === item.component,
+                  }"
+                  class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                >
+                  <span
+                    class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap"
+                    >{{ item.name }}</span
+                  >
+                  <span v-if="hasSubmenu(item.id)" class="mr-2">
+                    <svg
+                      :class="{
+                        'transform rotate-90': openSubmenu === item.id,
+                      }"
+                      class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 22 21"
+                    >
+                      <path
+                        d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"
+                      />
+                      <path
+                        d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                <!-- Render child menus -->
+                <ul v-if="openSubmenu === item.id">
+                  <template v-for="(child, childIndex) in menus">
+                    <li
+                      v-if="child.pivot.parent_id === item.id"
+                      :key="childIndex"
+                    >
+                      <button
+                        type="button"
+                        @click="loadComponent(child.component)"
+                        :class="{
+                          'bg-gray-100 dark:bg-gray-700':
+                            component_id === child.component,
+                        }"
+                        class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                      >
+                        <span
+                          class="flex-1 ms-6 text-left rtl:text-right whitespace-nowrap"
+                          >{{ child.name }}</span
+                        >
+                      </button>
+                    </li>
+                  </template>
                 </ul>
-            </div>
-        </aside>
-        <!-- Left Side End -->
-    </div>
+              </li>
+            </template>
+          </template>
+        </ul>
+        <ul
+          class="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700"
+        >
+          <nuxt-link class="ml-5" to="/profile">Profile</nuxt-link>
+        </ul>
+      </div>
+    </aside>
+    <!-- Left Side End -->
+  </div>
 </template>
+
 <style></style>
-  
--- client/components/TopMenu.vue
+++ client/components/TopMenu.vue
@@ -1,142 +1,189 @@
 <script setup>
 // Get Data from store
-const state_user_data = storeUserData()
-const { addLang } = state_user_data
+const state_user_data = storeUserData();
+const { addLang } = state_user_data;
 
 // Get Data Auth user
 const { user = {}, logout } = useAuth();
 
 // Change Language
 const { locale, t } = useI18n();
-const selected_lang = ref(state_user_data.lang ? state_user_data.lang : 'en')
-locale.value = selected_lang.value
+const state = storeUserData();
+
+const selected_lang = ref(state.lang ? state.lang : "en");
+locale.value = selected_lang.value;
 
 function changeLanguage() {
-	addLang(selected_lang.value)
-	locale.value = selected_lang.value
+  addLang(selected_lang.value);
+  locale.value = selected_lang.value;
 }
 
 // Toggle Language
 function ToggleChangeLanguage() {
-	if (selected_lang.value === 'en') {
-		selected_lang.value = 'bn'
-	} else {
-		selected_lang.value = 'en'
-	}
-	changeLanguage()
+  if (selected_lang.value === "en") {
+    selected_lang.value = "bn";
+  } else {
+    selected_lang.value = "en";
+  }
+  changeLanguage();
 }
 
 // closeSidebar make toggle button
-const sidebarToggler = ref(false)
+const sidebarToggler = ref(false);
 
 function closeSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    const dashboard_body = document.querySelector('.dashboard_close')
-    sidebar.classList.toggle('closed')
-    dashboard_body.classList.toggle('sm:ml-64')
-    sidebarToggler.value = !sidebarToggler.value
+  const sidebar = document.querySelector(".sidebar");
+  const dashboard_body = document.querySelector(".dashboard_close");
+  sidebar.classList.toggle("closed");
+  dashboard_body.classList.toggle("sm:ml-64");
+  sidebarToggler.value = !sidebarToggler.value;
 }
 
 const props = defineProps({
-    title: {
-        type: String,
-        // default: 'dashboard'
-    }
-})
+  title: {
+    type: String,
+    // default: 'dashboard'
+  },
+});
 </script>
 
 <template>
-    <div>
-        <!-- Top Menu Start-->
-        <div class="px-3 py-3 lg:px-5 lg:pl-3 bg-white dark:bg-gray-800 top_menu" 
-            :class="{
-            'dashboard': props.title === 'dashboard' && !sidebarToggler,
-            'innerPage': props.title !== 'dashboard' || (props.title === 'dashboard' && sidebarToggler)
-            }"
+  <div>
+    <!-- Top Menu Start-->
+    <div
+      class="px-3 py-3 lg:px-5 lg:pl-3 bg-white dark:bg-gray-800 top_menu"
+      :class="{
+        dashboard: props.title === 'dashboard' && !sidebarToggler,
+        innerPage:
+          props.title !== 'dashboard' ||
+          (props.title === 'dashboard' && sidebarToggler),
+      }"
+    >
+      <div class="flex items-center justify-between">
+        <div
+          class="flex items-center justify-start rtl:justify-end select-none"
         >
-            <div class="flex items-center justify-between">
-                <div class="flex items-center justify-start rtl:justify-end select-none">
-                    <button v-if="props.title == 'dashboard'" data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar"
-                        type="button"
-                        class="drawer_button inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                        <span class="sr-only">Open sidebar</span>
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path clip-rule="evenodd" fill-rule="evenodd"
-                                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-                            </path>
-                        </svg>
-                    </button>
+          <button
+            v-if="props.title == 'dashboard'"
+            data-drawer-target="logo-sidebar"
+            data-drawer-toggle="logo-sidebar"
+            aria-controls="logo-sidebar"
+            type="button"
+            class="drawer_button inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          >
+            <span class="sr-only">Open sidebar</span>
+            <svg
+              class="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+              ></path>
+            </svg>
+          </button>
 
-                    <button v-if="props.title == 'dashboard'" @click="closeSidebar" class="desktop_view">
-                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path clip-rule="evenodd" fill-rule="evenodd"
-                                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-                            </path>
-                        </svg>
-                    </button>
+          <button
+            v-if="props.title == 'dashboard'"
+            @click="closeSidebar"
+            class="desktop_view"
+          >
+            <svg
+              class="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+              ></path>
+            </svg>
+          </button>
 
-                    <a :href="'/' + props.title" class="topbar_title flex ms-2 md:me-24 select-none">
-                        <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white select-none">
-                            {{$t(props.title)}}
-                        </span>
-                    </a>
-                </div>
-                <div class="flex items-center">
-                    <div class="flex items-center ms-3">
-                        <div class="flex items-center justify-center">
-                            <label class="toggle mr-3">
-                                <input type="checkbox" :checked="selected_lang === 'en' ? true : false"
-                                    @click="ToggleChangeLanguage()">
-                                <span class="slider"></span>
-                                <span class="labels" :data-on="selected_lang === 'bn' ? 'bn' : 'BN'"
-                                    :data-off="selected_lang === 'en' ? 'en' : 'EN'"></span>
-                            </label>
-                        </div>
-                        <div>
-                            <button type="button"
-                                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                                aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                                <span class="sr-only">Open user menu</span>
-                                <img class="w-8 h-8 rounded-full" src="/assets/img/icon/profile.png" alt="user photo">
-                            </button>
-                        </div>
-
-                        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-                            id="dropdown-user">
-                            <div class="px-4 py-3" role="none">
-                                <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                    {{ state_user_data.user_data?.username }}
-                                </p>
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                    {{ state_user_data.user_data?.email }}
-                                </p>
-                            </div>
-                            <ul class="py-1" role="none">
-                                <li>
-                                    <a href="/profile"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Profile</a>
-                                </li>
-                                <li>
-                                    <a href="/dashboard"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Dashboard</a>
-                                </li>
-                                <li>
-                                    <a href="#" @click="logout"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Sign out</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <a
+            :href="'/' + props.title"
+            class="topbar_title flex ms-2 md:me-24 select-none"
+          >
+            <span
+              class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white select-none"
+            >
+              {{ $t(props.title) }}
+            </span>
+          </a>
         </div>
-        <!-- Top Menu End-->
+        <div class="flex items-center">
+          <div class="flex items-center ms-3">
+            <div class="flex items-center justify-center">
+              <label class="toggle mr-3">
+                <input
+                  type="checkbox"
+                  :checked="selected_lang === 'en' ? true : false"
+                  @click="ToggleChangeLanguage()"
+                />
+                <span class="slider"></span>
+                <span
+                  class="labels"
+                  :data-on="selected_lang === 'bn' ? 'bn' : 'BN'"
+                  :data-off="selected_lang === 'en' ? 'en' : 'EN'"
+                ></span>
+              </label>
+            </div>
+            <div>
+              <button
+                type="button"
+                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                aria-expanded="false"
+                data-dropdown-toggle="dropdown-user"
+              >
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="w-8 h-8 rounded-full"
+                  src="/assets/img/icon/profile.png"
+                  alt="user photo"
+                />
+              </button>
+            </div>
+
+            <div
+              class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+              id="dropdown-user"
+            >
+              <div class="px-4 py-3" role="none">
+                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                  {{ state_user_data.user_data?.username }}
+                </p>
+                <p
+                  class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                  role="none"
+                >
+                  {{ state_user_data.user_data?.email }}
+                </p>
+              </div>
+              <ul class="py-1" role="none">
+                <li>
+                  <a
+                    href="#"
+                    @click="logout"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                    role="menuitem"
+                    >Sign out</a
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    <!-- Top Menu End-->
+  </div>
 </template>
 
 <style></style>
